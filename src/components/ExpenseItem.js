c6b0c323391d6a79524f167ac5a5a2b0cd54b9c8
Matt Calimbas <matthewcalimbas@gmail.com>
2023-05-31 13:16:13 -0700

    added css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-
+import './ExpenseItem.css'
 
 function ExpenseItem() {
     return (
-        <div>
-            <div>Date</div>
-            <div>
-                <h2>Title</h2>
-                <div>Amount</div>
+        <div className='expense-item'>
+            <div>May 22, 2023</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$239.33</div>
             </div>
         </div>
     )
