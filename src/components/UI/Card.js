09e36ec3a9af13514ef2915602e405f6f5ede3f9
Matt Calimbas <matthewcalimbas@gmail.com>
2023-06-14 22:16:44 -0700

    card component comment

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.css'
 
+// Reuseable component with card specific css
 const Card = (props) => {
     const classes = `card ${props.className}`
     return (
