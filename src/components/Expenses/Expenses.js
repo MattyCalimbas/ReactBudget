3e04ba2b7503069fa8f1ffb080be5d5cbe3d2171
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-11 11:56:42 -0700

    removed derived state function, not needed for ultimate functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,27 +10,30 @@ const Expenses = ({ expenses }) => {
     // Depricated function for setting up a state that is dependant on a value of another state variable
     // const [filterInfoText, setFilterInfoText] = useState('2019, 2021 & 2022')
 
-    let filterInfoText = '2019,2021, & 2022'
+    // computed value derived from the value of the above state 
+    // let filterInfoText = '2019,2021, & 2022'
 
     // Experiment: Derived State Feature
     // State is depenedant upon filteredYear variable so rather than updating a seperate state variables, we simply compute the value of the filteredYear variable
-    if (filteredYear === '2019') {
-        filterInfoText = '2020, 2021 & 2022';
-    } else if (selectedYear === '2020') {
-        filterInfoText = '2019, 2021 & 2022';
-    } else if (selectedYear === '2021') {
-        filterInfoText = '2019, 2020 & 2022';
-    } else {
-        filterInfoText = '2019, 2020 & 2021';
-    }
 
+    // if (filteredYear === '2019') {
+    //     filterInfoText = '2020, 2021 & 2022';
+    // } else if (filteredYear === '2021') {
+    //     filterInfoText = '2019, 2020 & 2022';
+    // } else {
+    //     filterInfoText = '2019, 2020 & 2021';
+    // }
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear)
+    }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 {/* Experiment: Derived State Feature */}
-                <p>Data for years {filterInfoText} is hidden.</p>
+                {/* <p>Data for years {filterInfoText} is hidden.</p> */}
                 <ExpenseItem
                     title={expenses[0].title}
                     amount={expenses[0].amount}
