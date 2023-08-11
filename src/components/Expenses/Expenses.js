32cfa54a16592d4293ffc38e7267cd5e48e24b95
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-11 11:40:15 -0700

    experiment on derived state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,15 +7,28 @@ import ExpensesFilter from './ExpensesFilter';
 // Using array destructuring to seperate props from props.expenses
 const Expenses = ({ expenses }) => {
     const [filteredYear, setFilteredYear] = useState('2020');
+    const [filterInfoText, setFilterInfoText] = useState('2019, 2021, & 2022')
 
+    // Experiment: Derived State Feature
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear)
+        if(selectedYear === '2019') {
+            setFilterInfoText('2020, 2021 & 2022')
+        } else if (selectedYear === '2020' ){
+            setFilterInfoText('2019, 2021 & 2022')
+        } else if (selectedYear === '2021'){
+            setFilterInfoText('2019, 2020 & 2022')
+        } else {
+            setFilterInfoText('2019, 2020 & 2021')
+        }
     }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {/* Experiment: Derived State Feature */}
+                <p>Data for years {filterInfoText} is hidden.</p>
                 <ExpenseItem
                     title={expenses[0].title}
                     amount={expenses[0].amount}
