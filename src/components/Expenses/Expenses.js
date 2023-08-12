3542b22c396cc99683bff327d6a50b72c759d4ad
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-12 15:03:32 -0700

    mapping over expeneses to render expense items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -34,26 +34,7 @@ const Expenses = ({ expenses }) => {
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 {/* Experiment: Derived State Feature */}
                 {/* <p>Data for years {filterInfoText} is hidden.</p> */}
-                <ExpenseItem
-                    title={expenses[0].title}
-                    amount={expenses[0].amount}
-                    date={expenses[0].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={expenses[1].title}
-                    amount={expenses[1].amount}
-                    date={expenses[1].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={expenses[2].title}
-                    amount={expenses[2].amount}
-                    date={expenses[2].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={expenses[3].title}
-                    amount={expenses[3].amount}
-                    date={expenses[3].date}>
-                </ExpenseItem>
+                {expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
             </Card>
         </div>
     )
