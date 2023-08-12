380aa569b893220fb6ade9f7f1a2decf5e71c705
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-12 15:33:18 -0700

    added dynamic list functionality

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense"
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2023, 1, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2023, 3, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2023, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2023, 1, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2023, 3, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2023, 5, 12),
-    },
-  ];
+  // References item array above and sets it as state variable
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   // most common pattern to pass props upward
   // create custom prop and function on component that handles data being passed upward
+  // data is now argument to setExpenses useState function to dynamically add new item to list
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses(prevExpense => [expense, ...prevExpense])
   }
 
   return (
