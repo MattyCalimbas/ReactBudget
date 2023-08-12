0e5f290c88cfa90cd7560ae86cfce448b5957f7f
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-12 14:31:07 -0700

    removed function from expense item component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -8,27 +8,18 @@ const ExpenseItem = (props) => {
     // hooks can only be called directly within component functions
     // Returns an array of two elements.  
     // First element is state value and second element is a function that alters state value. 
-    const [title, setTitle] = useState(props.title);
+    // const [title, setTitle] = useState(props.title);
     
-    const clickHandler = () => {
-        setTitle('Updated!')
-        // console.log registeres first in console reflecting initial state.
-        // setTitle hook isn't called immediately but rather "scheduled"
-        // useState cycle: 1. register state with useState. Two values are returned, the value itself and the updating function. 2. Call updating function to change value 3. use first element to output state element.
-        // anytime state changes component reloads
-        console.log(title)
-    };
+
 
     return (
         // Sending expense-item class to Card component, can catch attributes with props object
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            {/* onClick calls clickHandler; replaces addEventListener functionality */}
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
