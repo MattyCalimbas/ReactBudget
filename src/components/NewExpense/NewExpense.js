79856ed534ed488ec41aa3179983a7a2dd7d0189
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-15 09:01:05 -0700

    added dynamic rendering functionality concerning New Expense component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
     // most common pattern to pass props upward
     // create custom prop and function on component that handles data being passed upward
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
@@ -11,15 +12,30 @@ const NewExpense = (props) => {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-        
+        setIsEditing(false)
         // call custom prop from parent component (App.js) and pass the data
         props.onAddExpense(expenseData)
     };
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
+    let editEl;
+
+    if(!isEditing){
+        editEl = <button onClick={startEditingHandler}>Add New Expense</button>
+    } else {
+        editEl = <ExpenseForm clickStopHandler={stopEditingHandler} onSaveExpenseData={onSaveExpenseDataHandler} />
+    }
 
     return (
         <div className="new-expense">
             {/* use custom prop to call function that will handle data being sent upward from ExpenseForm */}
-            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+            {/* <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} /> */}
+            {editEl}
         </div>
     )
 };
