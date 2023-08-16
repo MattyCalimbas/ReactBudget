59665ff13ed33a70f6b2dc5eba57a87cae2623fa
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-16 10:54:43 -0700

    initial functional coding for chartbar component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ChartBar from './ChartBar'
+import './ChartBar.css'
+
+const Chart = (props) => {
+    return (
+        <div className="chart">
+            {props.dataPoints.map(dataPoint => (
+                <ChartBar
+                    value={dataPoint.value}
+                    maxValue={null}
+                    label={dataPoint.label}
+                    key={dataPoint.id}
+                />))}
+        </div>
+    )
+};
+
+export default Chart
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ChartBar.css';
+
+const ChartBar = (props) => {
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar___inner">
+                <div className="chart-bar___fill"></div>
+            </div>
+            <div className="chart-bar___label">
+
+            </div>
+        </div>
+    )
+}
+
+export default ChartBar
