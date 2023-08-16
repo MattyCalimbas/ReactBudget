d9247ea95b97da26b8806f4ed4db548dbda63216
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-16 11:33:32 -0700

    added data point mapping logic to chart component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,12 +3,15 @@ import ChartBar from './ChartBar'
 import './ChartBar.css'
 
 const Chart = (props) => {
+    // Create a new array consisting of integers extracted from props.dataPoints
+    const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointsValues);
     return (
         <div className="chart">
             {props.dataPoints.map(dataPoint => (
                 <ChartBar
                     value={dataPoint.value}
-                    maxValue={null}
+                    maxValue={totalMaximum}
                     label={dataPoint.label}
                     key={dataPoint.id}
                 />))}
