e60df085956d51bb17edcfaee235a61b10a92a11
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-16 11:06:13 -0700

    logic for chart bar fill

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './ChartBar.css';
 
 const ChartBar = (props) => {
+
+    let barFillHeight = '0%';
+
+    if (props.max > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%'
+    }
     return (
         <div className="chart-bar">
             <div className="chart-bar___inner">
-                <div className="chart-bar___fill"></div>
+                {/* Example of setting dymnamic style using in-line css  */}
+                <div className="chart-bar___fill" style={{height:barFillHeight}}></div>
             </div>
-            <div className="chart-bar___label">
-
-            </div>
+            <div className="chart-bar___label">{props.label}</div>
         </div>
     )
 }
